ec942ef559cef216f1aec0ed24109c334bcf8823
Christian Gonzalvez <32960017+chriscatuk@users.noreply.github.com>
2025-04-14 20:53:16 +0100

    Update __init__.py

--- esphome/components/rainfall_sensor/__init__.py
+++ esphome/components/rainfall_sensor/__init__.py
@@ -1,27 +1,19 @@
-import esphome.codegen as cg
+from esphome.components import sensor
 import esphome.config_validation as cv
-from esphome.components import uart, sensor
-from esphome.const import CONF_ID, CONF_UART_ID
-
-DEPENDENCIES = ["uart"]
+import esphome.codegen as cg
 
 rainfall_sensor_ns = cg.esphome_ns.namespace("rainfall_sensor")
 RainfallSensor = rainfall_sensor_ns.class_(
-    "RainfallSensor", sensor.Sensor, cg.PollingComponent, uart.UARTDevice
+    "RainfallSensor", sensor.Sensor, cg.Component
 )
 
-CONFIG_SCHEMA = cv.Schema(
+CONFIG_SCHEMA = sensor.sensor_schema().extend(
     {
         cv.GenerateID(): cv.declare_id(RainfallSensor),
-        cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
     }
-).extend(cv.polling_component_schema("60s"))
+)
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await sensor.register_sensor(var, config)
+    var = await sensor.new_sensor(config)
     await cg.register_component(var, config)
-    await uart.register_uart_device(var, config[CONF_UART_ID])
-
-    cg.add(var.set_uart_parent(await cg.get_variable(config[CONF_UART_ID])))
