786dff58b158b5f1d515f88fb8e6c9043cae755f
Christian Gonzalvez <32960017+chriscatuk@users.noreply.github.com>
2025-04-14 20:52:07 +0100

    Update __init__.py

--- esphome/components/rainfall_sensor/__init__.py
+++ esphome/components/rainfall_sensor/__init__.py
@@ -1,23 +1,24 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart, sensor
-from esphome.const import CONF_ID, CONF_UART_ID
+from esphome.const import CONF_NAME, UNIT_MILLIMETER, ICON_WATER, DEVICE_CLASS_RAINFALL
 
-DEPENDENCIES = ['uart']
+rainfall_sensor_ns = cg.esphome_ns.namespace("rainfall_sensor")
+RainfallSensor = rainfall_sensor_ns.class_(
+    "RainfallSensor", sensor.Sensor, cg.Component, uart.UARTDevice
+)
 
-rainfall_sensor_ns = cg.esphome_ns.namespace('rainfall_sensor')
-RainfallSensor = rainfall_sensor_ns.class_('RainfallSensor', sensor.Sensor, cg.PollingComponent, uart.UARTDevice)
+CONFIG_SCHEMA = sensor.sensor_schema(
+    UNIT_MILLIMETER, ICON_WATER, 2, DEVICE_CLASS_RAINFALL
+).extend(
+    {
+        cv.GenerateID(): cv.declare_id(RainfallSensor),
+        cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
+    }
+)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(RainfallSensor),
-    cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-}).extend(cv.polling_component_schema('60s'))
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await sensor.register_sensor(var, config)
+    var = await sensor.new_sensor(config)
     await cg.register_component(var, config)
-    await uart.register_uart_device(var, config[CONF_UART_ID])
-
-    cg.add(var.set_uart_parent(await cg.get_variable(config[CONF_UART_ID])))
-
+    await uart.register_uart_device(var, config)
