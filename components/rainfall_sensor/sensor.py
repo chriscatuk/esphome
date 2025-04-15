7e79fc7215f496fd9ce1e5ac1427b0522ea59c58
Christian Gonzalvez <32960017+chriscatuk@users.noreply.github.com>
2025-04-15 23:30:48 +0100

    uart_id

--- components/rainfall_sensor/sensor.py
+++ components/rainfall_sensor/sensor.py
@@ -21,7 +21,7 @@ CONFIG_SCHEMA = (
     .extend(
         {
             cv.GenerateID(): cv.declare_id(RainfallSensor),
-            cv.GenerateID(uart.UART_ID): cv.use_id(uart.UARTComponent),
+            cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
         }
     )
     .extend(cv.polling_component_schema("60s"))
@@ -29,7 +29,7 @@ CONFIG_SCHEMA = (
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], config[uart.UART_ID])
+    var = cg.new_Pvariable(config[CONF_ID], config["uart_id"])
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
     await uart.register_uart_device(var, config)
