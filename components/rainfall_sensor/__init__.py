60713bb6e33409712f70052488c4ac2bf5c9e96b
Christian Gonzalvez <32960017+chriscatuk@users.noreply.github.com>
2025-04-17 23:07:33 +0100

    to_code in python

--- components/rainfall_sensor/__init__.py
+++ components/rainfall_sensor/__init__.py
@@ -1,14 +1,7 @@
-from esphome.components import uart
 import esphome.codegen as cg
+from esphome.components import uart
 
 rainfall_sensor_ns = cg.esphome_ns.namespace("rainfall_sensor")
 RainfallSensor = rainfall_sensor_ns.class_(
     "RainfallSensor", cg.Component, uart.UARTDevice
 )
-
-
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield sensor.register_sensor(var, config)
-    yield uart.register_uart_device(var, config[CONF_UART_ID])
--- components/rainfall_sensor/sensor.py
+++ components/rainfall_sensor/sensor.py
@@ -1,6 +1,7 @@
 from esphome.components import sensor
-from esphome.components.uart import UARTComponent, uart
+from esphome.components import uart
 import esphome.config_validation as cv
+import esphome.codegen as cg
 from esphome.const import (
     CONF_ID,
     CONF_NAME,
@@ -8,23 +9,23 @@ from esphome.const import (
     ICON_WATER,
     CONF_UART_ID,
 )
-from esphome import automation
-from esphome import pins
 
 from . import rainfall_sensor_ns, RainfallSensor
 
-RainfallSensor = rainfall_sensor_ns.class_(
-    "RainfallSensor", sensor.Sensor, uart.UARTDevice
-)
-
 CONFIG_SCHEMA = sensor.sensor_schema(
-    RainfallSensor,
     unit_of_measurement=UNIT_MILLIMETER,
     icon=ICON_WATER,
     accuracy_decimals=2,
 ).extend(
     {
         cv.GenerateID(): cv.declare_id(RainfallSensor),
-        cv.Required(CONF_UART_ID): cv.use_id(UARTComponent),
+        cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
     }
 )
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await sensor.register_sensor(var, config)
+    await uart.register_uart_device(var, config[CONF_UART_ID])
