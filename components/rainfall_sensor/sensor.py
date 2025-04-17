b31f4525579fbb8d7b593ec703b4fdb958d643a0
Christian Gonzalvez <32960017+chriscatuk@users.noreply.github.com>
2025-04-17 23:11:28 +0100

    Update sensor.py

--- components/rainfall_sensor/sensor.py
+++ components/rainfall_sensor/sensor.py
@@ -28,4 +28,4 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
-    await uart.register_uart_device(var, config[CONF_UART_ID])
+    await uart.register_uart_device(var, config.uart_id)
