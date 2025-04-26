83502c4b3ce664fb0680bf1165d5dd508c9e0f24
Christian Gonzalvez <32960017+chriscatuk@users.noreply.github.com>
2025-04-26 12:05:44 +0100

    debug

--- components/rainfall/sensor.py
+++ components/rainfall/sensor.py
@@ -3,13 +3,35 @@ import esphome.codegen as cg
 from esphome.components import sensor, uart
 import esphome.config_validation as cv
 from esphome.const import (
+    CONF_ID,
+    CONF_NAME,
     CONF_UART_ID,
+    CONF_UNIT_OF_MEASUREMENT,
+    CONF_ACCURACY_DECIMALS,
+    DEVICE_CLASS_PRECIPITATION,
+    DEVICE_CLASS_PRECIPITATION_INTENSITY,
+    STATE_CLASS_TOTAL_INCREASING,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_MILLIMETER,
+    ICON_WATER,
 )
 
+# Not available in ESPHome, so define it manually:
+# UNIT_MILLIMETER_PER_HOUR = "mm/h"
 
-CONFIG_SCHEMA = cv.Schema(
-    {cv.Optional(CONF_UART_ID): cv.use_id(uart.UARTComponent)}
-).extend(cv.COMPONENT_SCHEMA)
+# CODEOWNERS = ["@chriscatuk"]
+
+# MULTI_CONF = True
+
+# DEPENDENCIES = ["uart"]
+# AUTO_LOAD = ["sensor", "text_sensor"]
+
+# CONF_PRECIPITATION = "precipitation"
+# CONF_PRECIPITATION_INTENSITY = "precipitation_intensity"
+
+# CONFIG_SCHEMA = cv.Schema(
+#     {cv.Optional(CONF_UART_ID): cv.use_id(uart.UARTComponent)}
+# ).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
