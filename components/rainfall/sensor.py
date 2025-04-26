eca6f277897db1d5c99d1d98f6b00688e2ec7a70
Christian Gonzalvez <32960017+chriscatuk@users.noreply.github.com>
2025-04-26 11:56:21 +0100

    debug

--- components/rainfall/sensor.py
+++ components/rainfall/sensor.py
@@ -2,6 +2,52 @@ from esphome import pins
 import esphome.codegen as cg
 from esphome.components import sensor, uart
 import esphome.config_validation as cv
+from esphome.const import (
+    CONF_ID,
+    CONF_NAME,
+    CONF_UART_ID,
+    CONF_UNIT_OF_MEASUREMENT,
+    CONF_ACCURACY_DECIMALS,
+    DEVICE_CLASS_PRECIPITATION,
+    DEVICE_CLASS_PRECIPITATION_INTENSITY,
+    STATE_CLASS_TOTAL_INCREASING,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_MILLIMETER,
+    ICON_WATER,
+)
+
+# Not available in ESPHome, so define it manually:
+UNIT_MILLIMETER_PER_HOUR = "mm/h"
+
+CODEOWNERS = ["@chriscatuk"]
+
+MULTI_CONF = True
+
+DEPENDENCIES = ["uart"]
+AUTO_LOAD = ["sensor", "text_sensor"]
+
+CONF_PRECIPITATION = "precipitation"
+CONF_PRECIPITATION_INTENSITY = "precipitation_intensity"
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.Optional(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+        cv.Optional(CONF_PRECIPITATION): sensor.sensor_schema(
+            unit_of_measurement=UNIT_MILLIMETER,
+            accuracy_decimals=2,
+            device_class=DEVICE_CLASS_PRECIPITATION,
+            state_class=STATE_CLASS_TOTAL_INCREASING,
+            icon=ICON_WATER,
+        ),
+        cv.Optional(CONF_PRECIPITATION_INTENSITY): sensor.sensor_schema(
+            unit_of_measurement=UNIT_MILLIMETER_PER_HOUR,
+            accuracy_decimals=2,
+            device_class=DEVICE_CLASS_PRECIPITATION_INTENSITY,
+            state_class=STATE_CLASS_MEASUREMENT,
+            icon=ICON_WATER,
+        ),
+    }
+).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
