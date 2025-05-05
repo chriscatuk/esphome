47b24e6d276ec6fbd5747179b08e723efb49fcad
Christian Gonzalvez <32960017+chriscatuk@users.noreply.github.com>
2025-05-05 20:07:03 +0100

    rewrite

--- components/sen0575/sen0575.cpp
+++ components/sen0575/sen0575.cpp
@@ -8,14 +8,13 @@ namespace esphome
 
         static const char *const TAG = "sen0575";
 
-        // Constructor
-        Sen0575::Sen0575(uart::UARTComponent *parent) : PollingComponent(1000), UARTDevice(parent) {}
-
         // `setup()` is where you can initialize components if necessary
         void Sen0575::setup()
         {
             ESP_LOGCONFIG(TAG, "Setting up SEN0575 Rainfall Sensor...");
-            // Add any required initialization for the sensor here.
+            _mode = mode;
+            pid = 0;
+            vid = 0;
         }
 
         // `update()` will be called every `PollingComponent` interval
@@ -60,6 +59,75 @@ namespace esphome
         float Sen0575::get_setup_priority() const
         {
             return setup_priority::DATA;
+        }
+
+        bool Sen0575::begin(void)
+        {
+            return getPidVid();
+        }
+
+        std::string Sen0575::getFirmwareVersion(void)
+        {
+            uint16_t version = 0;
+            version = readRegister(eInputRegVersionSEN0575);
+            return std::to_string(version >> 12) + "." +
+                   std::to_string((version >> 8) & 0x0F) + "." +
+                   std::to_string((version >> 4) & 0x0F) + "." +
+                   std::to_string(version & 0x0F);
+        }
+
+        bool Sen0575::getPidVid(void)
+        {
+            bool ret = false;
+            pid = readRegister(eInputRegPidSEN0575);
+            vid = readRegister(eInputRegVidSEN0575);
+            pid = (vid & 0xC000) << 2 | pid;
+            vid = vid & 0x3FFF;
+            if ((vid == 0x3343) && (pid == 0x100C0))
+            {
+                ret = true;
+            }
+            return ret;
+        }
+
+        float Sen0575::getRainfall(void)
+        {
+            uint32_t rainfall = 0;
+            rainfall = readRegister(eInputRegCumulativeRainFallHSEN0575);
+            rainfall = rainfall << 16 | readRegister(eInputRegCumulativeRainFallLSEN0575);
+            return rainfall / 10000.0;
+        }
+
+        float Sen0575::getRainfall(uint8_t hour)
+        {
+            uint32_t rainfall = 0;
+            writeRegister(eHoldingRegRainHourSEN0575, hour);
+            rainfall = readRegister(eInputRegTimeRainFallHSEN0575);
+            rainfall = rainfall << 16 | readRegister(eInputRegTimeRainFallLSEN0575);
+            return rainfall / 10000.0;
+        }
+
+        uint32_t Sen0575::getRawData(void)
+        {
+            uint32_t rawdata = 0;
+            rawdata = readRegister(eInputRegRawDataHSEN0575);
+            rawdata = rawdata << 16 | readRegister(eInputRegRawDataLSEN0575);
+            return rawdata;
+        }
+
+        uint8_t Sen0575::setRainAccumulatedValue(float value)
+        {
+            uint8_t ret = 0;
+            uint16_t data = value * 10000;
+            ret = writeRegister(eHoldingRegBaseRainFallSEN0575, data);
+            return ret;
+        }
+
+        float Sen0575::getSensorWorkingTime(void)
+        {
+            uint16_t WorkingTime = 0;
+            WorkingTime = readRegister(eInputRegSysWorkingTimeSEN0575);
+            return WorkingTime / 60.0;
         }
     } // namespace sen0575
 } // namespace esphome
--- components/sen0575/sen0575.h
+++ components/sen0575/sen0575.h
@@ -3,7 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome.h"
+#include <string>
 
 namespace esphome
 {
@@ -13,8 +13,6 @@ namespace esphome
         class Sen0575 : public PollingComponent, public uart::UARTDevice
         {
         public:
-            Sen0575(uart::UARTComponent *parent);
-
             void setup() override;
             void update() override; // Declare the update method here
 
@@ -22,6 +20,8 @@ namespace esphome
 
             void set_total_rainfall(float rainfall) { total_rainfall_ = rainfall; }
             void set_precipitation_intensity(float intensity) { precipitation_intensity_ = intensity; }
+            float get_total_rainfall() { return total_rainfall; }
+            float get_precipitation_intensity() { return recipitation_intensity_; }
 
             void dump_config() override;
             float get_setup_priority() const override;
@@ -29,12 +29,101 @@ namespace esphome
             void set_precipitation_sensor(sensor::Sensor *precipitation_sensor) { precipitation_sensor_ = precipitation_sensor; }
             void set_precipitation_intensity_sensor(sensor::Sensor *precipitation_intensity_sensor) { precipitation_intensity_sensor_ = precipitation_intensity_sensor; }
 
+            /**
+             * @enum  eSEN0575InputReg_t
+             * @brief  Input Register Address
+             */
+            typedef enum
+            {
+                eInputRegPidSEN0575 = 0x0000,        /**< SEN0575 stores the address of the input register for PID in memory. */
+                eInputRegVidSEN0575,                 /**< The address of the input register for VID in memory. */
+                eInputRegRegAddrSEN0575,             /**< The address of the input register for device address in memory. */
+                eInputRegBaudSEN0575,                /**< The address of the input register for device baudrate in memory. */
+                eInputRegVerifyAndStopSEN0575,       /**< The address of the input register for RS485 parity bit and stop bit in memory. */
+                eInputRegVersionSEN0575,             /**< The address of the input register for firmware version in memory. */
+                eInputRegTimeRainFallLSEN0575,       /**< The address of the input register for low 16-bit cumulative rainfall in set time. */
+                eInputRegTimeRainFallHSEN0575,       /**< The address of the input register for high 16-bit cumulative rainfall in set time. */
+                eInputRegCumulativeRainFallLSEN0575, /**< The address of the input register for low 16-bit cumulative rainfall since working started. */
+                eInputRegCumulativeRainFallHSEN0575, /**< The address of the input register for high 16-bit cumulative rainfall since working started. */
+                eInputRegRawDataLSEN0575,            /**< The address of the input register for raw data (low 16-bit) in memory. */
+                eInputRegRawDataHSEN0575,            /**< he address of the input register for raw data (high 16-bit) in memory. */
+                eInputRegSysWorkingTimeSEN0575,      /**< The address of the input register for system working time in memory. */
+            } eSEN0575InputReg_t;
+
+            /**
+             * @enum  eSEN0575HoldingReg_t
+             * @brief  Holding Register Address of the Device
+             */
+            typedef enum
+            {
+                eHoldingRegReserved0SEN0575 = 0x0000, /**< SEN0575 this register is reserved. */
+                eHoldingRegReserved1SEN0575,          /**< SEN0575 this register is reserved. */
+                eHoldingRegReserved2SEN0575,          /**< SEN0575 this register is reserved. */
+                eHoldingRegReserved3SEN0575,          /**< SEN0575 this register is reserved. */
+                eHoldingRegReserved4SEN0575,          /**< SEN0575 this register is reserved. */
+                eHoldingRegReserved5SEN0575,          /**< SEN0575 this register is reserved. */
+                eHoldingRegRainHourSEN0575,           /**< Set the time to calculate cumulative rainfall */
+                eHoldingRegBaseRainFallSEN0575,       /**< Set the base rainfall value. */
+            } eSEN0575HoldingReg_t;
+
+            /**
+             * @fn begin
+             * @brief This function will attempt to communicate with a slave device and determine if the communication is successful based on the return value.
+             * @return Communication result
+             * @retval true  Succeed
+             * @retval false Failed
+             */
+            bool begin(void);
+
+            /**
+             * @fn getFirmwareVersion
+             * @brief  get firmware version
+             * @return  Return  firmware version
+             */
+            std::string getFirmwareVersion(void);
+
+            /**
+             * @fn getRainfall
+             * @brief Get cumulative rainfall
+             * @return Cumulative rainfall value
+             */
+            float getRainfall(void);
+
+            /**
+             * @fn getRainfall
+             * @brief Get the cumulative rainfall within the specified time
+             * @param hour Specified time (valid range is 1-24 hours)
+             * @return Cumulative rainfall
+             */
+            float getRainfall(uint8_t hour);
+
+            /**
+             * @fn getRawData
+             * @brief Get the Rawdata object
+             * @return Number of tipping bucket counts, unit: count
+             */
+            uint32_t getRawData();
+
+            /**
+             * @fn getSensorWorkingTime
+             * @brief Obtain the sensor working time
+             * @return Working time of the sensor, in hours
+             */
+            float getSensorWorkingTime();
+
+            /**
+             * @fn setRainAccumulatedValue
+             * @brief Set the Rain Accumulated Value object
+             * @param accumulatedValue Rainfall accumulation value, in millimeters
+             * @return Set result
+             * @retval 0  set successfully
+             * @retval Other values set failed
+             */
+            uint8_t setRainAccumulatedValue(float accumulatedValue = 0.2794);
+            uint32_t vid;
+            uint32_t pid;
+
         protected:
-            UARTComponent *uart_;
-
-            Sensor *total_rainfall_sensor_{nullptr};
-            Sensor *intensity_sensor_{nullptr};
-
             float total_rainfall_ = 0.0;
             float precipitation_intensity_ = 0.0;
 
@@ -42,6 +131,23 @@ namespace esphome
 
             sensor::Sensor *precipitation_sensor_{nullptr};
             sensor::Sensor *precipitation_intensity_sensor_{nullptr};
+
+        private:
+            int _dePin;
+            uint8_t _mode;
+
+            /**
+             * @fn getPidVid
+             * @brief  Get VID and PID
+             * @return  Result of getting the VID and PID
+             * @retval true:succeeded in getting the VID and PID
+             * @retval false:failed to get the data or the data obtained is incorrect
+             */
+            bool getPidVid(void);
+            virtual uint8_t readRegister(uint8_t reg, void *pBuf, size_t size) { return 0; };
+            virtual uint16_t readRegister(uint16_t reg) { return 0; };
+            virtual uint8_t writeRegister(uint8_t reg, void *pBuf, size_t size) { return 0; };
+            virtual uint16_t writeRegister(uint16_t reg, uint16_t data) { return 0; };
         };
 
     } // namespace dht12
