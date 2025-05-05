918ba2525f6fac93c693ced7d518a15e69b981c2
Christian Gonzalvez <32960017+chriscatuk@users.noreply.github.com>
2025-05-05 20:08:26 +0100

    total_rainfall_

--- components/sen0575/sen0575.h
+++ components/sen0575/sen0575.h
@@ -20,7 +20,7 @@ namespace esphome
 
             void set_total_rainfall(float rainfall) { total_rainfall_ = rainfall; }
             void set_precipitation_intensity(float intensity) { precipitation_intensity_ = intensity; }
-            float get_total_rainfall() { return total_rainfall; }
+            float get_total_rainfall() { return total_rainfall_; }
             float get_precipitation_intensity() { return recipitation_intensity_; }
 
             void dump_config() override;
